505876c376f87d628b13bb96360940e1d37cdec7
JinK001 <kyj9447@gmail.com>
2026-01-05 20:32:50 +0900

    delete web

--- mobile-web/src/pages/meetings/SocialEditPage.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { SocialEditPage as SocialFormEdit } from './SocialCreatePage';
-
-const SocialEditPage = () => <SocialFormEdit />;
-
-export default SocialEditPage;
-
