35c00e23da4d91121d8ef6ba1e86cceecd570eac
JinK001 <kyj9447@gmail.com>
2026-02-13 20:54:11 +0900

    RUN-9 <fix> (Club): 가입 신청, 회원 목록 등 누락부분 수정

--- app/_layout.js
+++ app/_layout.js
@@ -11,6 +11,7 @@ import { useEffect, useRef } from 'react';
 import { Platform, StyleSheet, View } from 'react-native';
 import { SafeAreaProvider, useSafeAreaInsets } from 'react-native-safe-area-context';
 
+import DebugConsoleOverlay from '@/components/debug/DebugConsoleOverlay';
 import BottomNavigationBar, { bottomNavHeight } from '@/components/layout/BottomNavigationBar';
 import FullMenu from '@/components/layout/FullMenu';
 import { AppLayoutProvider } from '@/context/AppLayoutContext';
@@ -140,7 +141,7 @@ function AppShell() {
       <BottomNavigationBar />
       <FullMenu />
       {/* !!!!!!!!!!!!!!!!!!!!! 디버그 오버레이 TODO 출시시 삭제 !!!!!!!!!!!!!!!!!!!!!! */}
-      {/* <DebugConsoleOverlay /> */}
+      <DebugConsoleOverlay />
       {/* !!!!!!!!!!!!!!!!!!!!! 디버그 오버레이 !!!!!!!!!!!!!!!!!!!!!! */}
     </View>
   );
