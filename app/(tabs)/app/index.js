e4b8f0899b0231a66fca23e2602763d50504f5a6
JinK001 <kyj9447@gmail.com>
2026-03-08 16:05:17 +0900

    RUN-70 <fix> : 이전 커밋 반영 취소

--- app/(tabs)/app/index.js
+++ app/(tabs)/app/index.js
@@ -5,7 +5,6 @@ import { useCallback, useEffect, useMemo, useRef, useState } from 'react';
 import {
   ActivityIndicator,
   ImageBackground,
-  Platform,
   Pressable,
   ScrollView,
   StyleSheet,
@@ -269,7 +268,6 @@ export default function HomeScreen() {
             pagingEnabled
             maxScrollDistancePerSwipe={carouselWidth}
             scrollAnimationDuration={CAROUSEL_SCROLL_ANIMATION_DURATION}
-            style={Platform.OS === 'web' ? { touchAction: 'pan-y' } : undefined}
             onConfigurePanGesture={(pan) => {
               pan.activeOffsetX([-12, 12]).failOffsetY([-8, 8]);
             }}
